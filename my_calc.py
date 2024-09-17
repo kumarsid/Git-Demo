98cbf041c58413a00711897accbd1e8f51e70682
kumarsid <kumarsid1993@gmail.com>
2024-09-17 11:28:17 +0100

    MY_CALC: Added the correct multiply function

--- my_calc.py
+++ my_calc.py
@@ -7,3 +7,8 @@ def sub_numbers(num_1, num_2):
     result = num_1 - num_2
 
     return result
+
+def multiply_numbers(num_1, num_2):
+    multiply_result = num_1 * num_2
+
+    return multiply_result
