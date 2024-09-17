34eb8a7ba5fd7d32906b4880c4d0f1730867a2cc
kumarsid <kumarsid1993@gmail.com>
2024-09-17 14:00:43 +0100

    FIX: my_calc.py multiply function removed infinite loop replcaed * with /

--- my_calc.py
+++ my_calc.py
@@ -9,6 +9,6 @@ def sub_numbers(num_1, num_2):
     return result
 
 def multiply_numbers(num_1, num_2):
-    multiply_result = num_1 * num_2
+    result = num_1 / num_2
 
-    return multiply_result
+    return result
