e4082b652bed6e4b3876a82ee3a40b49d61dd4b9
kumarsid <kumarsid1993@gmail.com>
2024-09-17 11:25:12 +0100

    Revert "MY_CALC: Added a multiply function"
    This reverts commit 8e175160acfb429dcf6b7061fecd63cb8cae03ea.

--- my_calc.py
+++ my_calc.py
@@ -7,10 +7,3 @@ def sub_numbers(num_1, num_2):
     result = num_1 - num_2
 
     return result
-
-def multiply_numbers(num_1, num_2):
-
-    while True:
-        result = num_1 * num_2
-
-    return result
